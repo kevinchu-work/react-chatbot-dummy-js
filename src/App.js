c81330ab271d6c65c47c75589ae749eda045251e
Kevin <kevinchu.work@gmail.com>
2019-12-08 22:26:28 +0800

    Master State & Default Props

--- src/App.js
+++ src/App.js
@@ -9,15 +9,51 @@ import InputBar         from './Components/InputBar';
 
 export default class App extends React.Component {
 
+  state = {
+    // Modules Control
+    is_mobileMenuOpened: false,
+    is_userLogined: false,
+    is_assistantBarShowing: false,
+
+    // User status
+    is_typing: false,
+    is_recording: false,  // Voice Record
+
+    // configration
+    interfaceLanguage: 'en',
+    is_demo: true
+
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.userLoginToggle = this.userLoginToggle.bind(this);
+  }
+
+  userLoginToggle = (loginedFlag) => {
+    this.setState({
+      is_userLogined: loginedFlag
+    });
+  }
+
   render() {
-    return 
+
+    let is_userLogined_var = this.state.is_userLogined;
+
+    return (
       <>
-        <Header id="header"></Header>
-        <SubTitle id="subtitle"></SubTitle>
-        <DialogWrapper id="root"></DialogWrapper>
-        <AssistantBar id="assistantBar"></AssistantBar>
-        <InputBar id="userTalk"></InputBar>
-      </>;
+        <header>
+          <Header id="header"></Header>
+          <SubTitle id="subtitle" is_userLogined={is_userLogined_var}></SubTitle>
+        </header>
+        <main>
+          <DialogWrapper id="root"></DialogWrapper>
+          <AssistantBar id="assistantBar"></AssistantBar>
+          <InputBar id="userTalk"></InputBar>
+        </main>
+      </>
+    );
   }
 
 }
